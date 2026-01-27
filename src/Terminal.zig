6cb757b26e4a5c6418ec7c83e445ecd78b3cd3ca
FObersteiner <f.obersteiner@posteo.de>
2026-01-27 14:13:59 +0100

    simplify terminal wrapper

--- src/Terminal.zig
+++ src/Terminal.zig
@@ -1,3 +1,6 @@
+//! wrapper around std.Io.Terminal that gives us a convenient
+//! init and print method.
+
 const Terminal = @This();
 
 const std = @import("std");
@@ -6,14 +9,12 @@ const ColorScheme = @import("ColorScheme.zig");
 const tty = std.Io.Terminal;
 const File = std.Io.File;
 
-writer: *std.Io.Writer,
-t: tty,
+instance: tty,
 
 pub fn init(io: std.Io, file: File, writer: *std.Io.Writer) !Terminal {
     const terminal_mode: std.Io.Terminal.Mode = try .detect(io, file, false, false);
     return .{
-        .writer = writer,
-        .t = .{ .writer = writer, .mode = terminal_mode },
+        .instance = .{ .writer = writer, .mode = terminal_mode },
     };
 }
 
@@ -24,12 +25,12 @@ pub fn print(
     args: anytype,
 ) void {
     for (style) |color| {
-        terminal.t.setColor(color) catch {};
+        terminal.instance.setColor(color) catch {};
     }
 
-    terminal.writer.print(format, args) catch {};
+    terminal.instance.writer.print(format, args) catch {};
 
     if (style.len > 0) {
-        terminal.t.setColor(.reset) catch {};
+        terminal.instance.setColor(.reset) catch {};
     }
 }
